0387807d04adb17c7a748ae8c1da719d22606707
yogieshwar <konneyogie@gmail.com>
2025-07-04 21:00:19 +0530

    router update

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { Authprovider } from './context/AuthContext.jsx'
 import { RoadmapProvider } from './context/RoadmapContext.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <RoadmapProvider>
     <Authprovider>
       <App />
     </Authprovider>
     </RoadmapProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
